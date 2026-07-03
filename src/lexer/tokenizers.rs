c97bbf0fee07e1c7ddb5456b6e77d5e0f8a59313
AbolfazlPourisa <abolfazlpourisa@gmail.com>
2026-07-03 21:35:28 +0000

    Added tokenizers

---
+++ src/lexer/tokenizers.rs
@@ -0,0 +1,107 @@
+use super::token::Tokens;
+use super::lexer::Lexer;
+
+impl Lexer {
+    pub fn tokenizer_char(&mut self) {
+        let mut ident = String::new();
+
+        while self.i < self.chars.len() && (self.chars[self.i].is_alphabetic() || self.chars[self.i] == '_') {
+            ident.push(self.chars[self.i]);
+
+            self.i += 1;
+        }
+
+        match ident.as_str() {
+            "let" => {
+                self.add_let();
+
+                return;
+            },
+
+            "and" => {
+                self.add_and();
+                
+                return;
+            },
+
+            "or" => {
+                self.add_or();
+            
+                return;
+            }
+
+            _ => {
+                self.add_identifier(ident);
+
+                return
+            }
+        }
+    }
+
+    pub fn tokenizer_number(&mut self) {
+        let mut number = String::new();
+        let mut is_float = false; 
+
+        while self.i < self.chars.len() {
+            let ch = self.chars[self.i];
+
+            if ch.is_digit(10) {
+                number.push(ch);
+
+                self.i += 1;
+
+                continue;
+            }
+
+            if ch == '.' {
+                let has_next = self.i + 1 < self.chars.len();
+                let mut next_is_digit = false;
+                if has_next {
+                    next_is_digit = self.chars[self.i + 1].is_digit(10);
+                }
+
+                let has_prev = self.i > 0;
+                let mut prev_is_digit = false;
+                if has_prev {
+                    prev_is_digit = self.chars[self.i - 1].is_digit(10);
+                }
+
+                if !is_float && (prev_is_digit && next_is_digit) {
+                    is_float = true;
+                    
+                    number.push(ch);
+
+                    self.i += 1;
+
+                    continue;
+                } else {
+                    panic!("Invalid float");
+                }
+            }
+
+            break;
+        }
+
+        if is_float {
+            match self.add_type_float(number) {
+                Ok(_) => {
+                    return;
+                },
+
+                Err(err) => {
+                    panic!("{}", err);
+                }
+            }
+        }
+
+        match self.add_type_int(number) {
+                Ok(_) => {
+                    return;
+                },
+
+                Err(err) => {
+                    panic!("{}", err);
+                }
+        }
+    }
+}
